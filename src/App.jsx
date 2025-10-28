7b97a2fa133c04689942a8ca5452253d35c6adb2
kosterseb <sebkos.code@gmail.com>
2025-10-28 09:17:42 +0100

    added dependencies and configs for deployment to gh

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { DarkModeProvider } from './contexts/DarkModeContext'
 import { ArchiveProvider } from './contexts/ArchiveContext'
 
